7d773c08a380c80381031ebb3711dec26e4fed5d
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-03-22 21:28:52 -0500

    new stuff

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ let mysql = require('mysql');
 let connection;
 
 if (process.env.JAWSDB_URL) {
+  console.log(process.env.JAWSDB_URL)
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'localhost',
     port: 3306,
-    user: 'reap7b74wa9dwg3n',
-    password: 'rtnstkun1ggb5zt5',
-    database: 'q8x7rbdt2fa9hup2'
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
   });
 }
 
