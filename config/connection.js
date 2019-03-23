6e187f13efacba3f9dca6fa25d06fcde318e8959
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-03-22 20:58:20 -0500

    new styles

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db'
-});
+let connection;
 
-connection.connect((err) =>{
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
 
-module.exports = connection;
+  connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,38 +1,38 @@
-let connection = require ('./connection.js');
+let connection = require('./connection.js');
 
 
 let orm = {
-	selectAll:(tableInput, cb) =>{
-		let queryString = "SELECT * FROM " + tableInput + ";";
-		connection.query(queryString, (err, result) =>{
-			if (err) {
-				throw err;
-			}
-			cb(result);
-		});
+    selectAll: (tableInput, cb) => {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
-    
 
-	addBurger: (vals, cb) =>{
+
+    addBurger: (vals, cb) => {
         console.log("hey there")
         let queryString = "INSERT INTO burgers (burger_name) VALUE (?)";
-		connection.query(queryString, vals, (err, result) =>{
-			if (err) {
-				throw err;
-			}
-    		cb(result);
-    	});
-	},
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
-	updateBurger: (id, cb) =>{
-		let queryString = "UPDATE  burgers SET devoured = true WHERE id = " + id
-		connection.query(queryString, (err, result) =>{
-			if (err) {
-				throw err;
-			}
-			cb(result);
-		});
-	}
+    updateBurger: (id, cb) => {
+        let queryString = "UPDATE  burgers SET devoured = true WHERE id = " + id
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 };
 
 module.exports = orm;
