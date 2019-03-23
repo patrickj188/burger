12e6999955e543149b43e6164859da3b4c0fa722
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-03-22 21:18:39 -0500

    things

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
+    user: 'reap7b74wa9dwg3n',
+    password: 'rtnstkun1ggb5zt5',
+    database: 'q8x7rbdt2fa9hup2'
   });
 }
 
